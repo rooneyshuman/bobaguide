f6856ce1b6f1624a5d50a8128c62d3c6a7011bf0
Carissa Allen <carissawood@outlook.com>
2019-11-12 18:34:07 -0800

    Rename model folder to reflect boba guide model
    The previous model folder, 'gbmodel' is for the guestbook model not used
    in this application. Updated to 'bgmodel' to reflect boba guide model.

--- shops.py
+++ shops.py
@@ -2,7 +2,7 @@ from __future__ import print_function # In python 2.7
 
 from flask import redirect, request, url_for, render_template
 from flask.views import MethodView
-import gbmodel
+import bgmodel
 import sys
 
 class Shops(MethodView):
@@ -11,7 +11,7 @@ class Shops(MethodView):
         GET method for the shops page
         :return: renders the shops.html page on return
         """
-        model = gbmodel.get_model()
+        model = bgmodel.get_model()
         shops = [dict(name=row[0], street=row[1], city=row[2], state=row[3], zip=row[4], open_hr=row[5],
                         close_hr=row[6], phone=row[7], drink=row[8], rating=row[9], website=row[10]) for row in model.select()]
         return render_template('shops.html', shops=shops)
@@ -21,7 +21,7 @@ class Shops(MethodView):
         POST method for the shops page. Deletes an entry from the db when called.
         :return: renders the shops.html page on return
         """
-        model = gbmodel.get_model()
+        model = bgmodel.get_model()
         model.delete(request.form['name'])
 
         return redirect(url_for('shops'))
