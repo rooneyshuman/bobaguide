683c14f13d72731cde2d682016911d73c7e05e59
Carissa Allen <carissawood@outlook.com>
2019-11-20 14:25:06 -0800

    Fix syntax error

--- bgmodel/__init__.py
+++ bgmodel/__init__.py
@@ -2,12 +2,12 @@ from .model_datastore import model
 from .settings_datastore import settings
 
 
-appmodel = model()	appmodel = model()
+appmodel = model()
 appsettings = settings()
 
 
-def get_model():	def get_model():
-    return appmodel 	    return appmodel
+def get_model():
+    return appmodel
 
 def get_settings():
     return appsettings
