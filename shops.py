b738551f135be6537afaf0c52555c599c8b6e56f
Belen Bustamante <rooneyshuman@gmail.com>
2019-11-20 18:40:29 -0800

    Removed unnecessary loop in api key retrieval, refactored name for google key column

--- shops.py
+++ shops.py
@@ -15,7 +15,8 @@ class Shops(MethodView):
         settings = bgmodel.get_settings()
         shops = [dict(name=row[0], street=row[1], city=row[2], state=row[3], zip=row[4], open_hr=row[5],
                         close_hr=row[6], phone=row[7], drink=row[8], rating=row[9], website=row[10]) for row in model.select()]
-        api_keys = [dict(api_key=row[0]) for row in settings.select()]
+        key_row = settings.select()[0]
+        api_keys = dict(google=key_row[0])
         return render_template('shops.html', shops=shops, api_keys=api_keys)
 
     def post(self):
