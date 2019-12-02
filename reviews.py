23f79cc8a616eba4f0dacb22305ab2d62c400511
Belen Bustamante <rooneyshuman@gmail.com>
2019-12-02 15:57:00 -0800

    Add check for proper response from yelp api

--- reviews.py
+++ reviews.py
@@ -19,22 +19,26 @@ class Reviews(MethodView):
 
         # Retrieve yelp id through phone search
         response = requests.get("https://api.yelp.com/v3/businesses/search/phone?phone=+1" + shop_phone, 
-            headers={"Authorization": "Bearer " + api_keys['yelp']}).json()
-        yelp_id = response['businesses'][0]['id']
-        
-        # Retrieve yelp reviews through yelp id and passes them to reviews template
-        raw_reviews = requests.get("https://api.yelp.com/v3/businesses/" + yelp_id + "/reviews", 
-            headers={"Authorization": "Bearer " + api_keys['yelp']}).json()['reviews']
-        
-        # Format datetime string to mm/dd/yyyy
-        for review in raw_reviews:
-            date_time_str = review['time_created']
-            formatted_date = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S').date()
-            review['time_created'] = str(formatted_date.month) + "/" + str(formatted_date.day) + "/" + str(formatted_date.year)
+            headers={"Authorization": "Bearer " + api_keys['yelp']})
 
-        yelp_reviews = [dict(id=review['id'], text=review['text'], date=review['time_created'], url=review['url'], 
-            user_name=review['user']['name'], user_img=review['user']['image_url']) for review in raw_reviews]
-        return render_template('reviews.html', yelp_reviews=yelp_reviews, shop_name=shop_name)
+        if(response.status_code == 200 and response.json()['total'] > 0):
+            yelp_id = response.json()['businesses'][0]['id']
+        
+            # Retrieve yelp reviews through yelp id and passes them to reviews template
+            raw_reviews = requests.get("https://api.yelp.com/v3/businesses/" + yelp_id + "/reviews", 
+                headers={"Authorization": "Bearer " + api_keys['yelp']}).json()['reviews']
+            
+            # Format datetime string to mm/dd/yyyy
+            for review in raw_reviews:
+                date_time_str = review['time_created']
+                formatted_date = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S').date()
+                review['time_created'] = str(formatted_date.month) + "/" + str(formatted_date.day) + "/" + str(formatted_date.year)
+
+            yelp_reviews = [dict(id=review['id'], text=review['text'], date=review['time_created'], url=review['url'], 
+                user_name=review['user']['name'], user_img=review['user']['image_url']) for review in raw_reviews]
+            return render_template('reviews.html', yelp_reviews=yelp_reviews, shop_name=shop_name)
+        else:
+            return render_template('404.html', shop_name=shop_name)
 
     def post(self):
         """
