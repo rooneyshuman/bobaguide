b36f7b2d55ef890e099f84d25f9827ef3622d63a
rooneyshuman <rooneyshuman@gmail.com>
2019-12-03 01:26:20 -0800

    Reformat all python files using Black

--- index.py
+++ index.py
@@ -3,10 +3,11 @@ from flask.views import MethodView
 from bgmodel.settings_datastore import Settings
 import sys
 
+
 class Index(MethodView):
     def get(self):
         """
         GET method for the main landing page
         :return: renders the index.html page on return
         """
-        return render_template('index.html')
+        return render_template("index.html")
