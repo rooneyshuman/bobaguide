3c2cf188142b923569bcdfbb7b269967acd565cd
rooneyshuman <rooneyshuman@gmail.com>
2019-12-03 00:20:01 -0800

    Reformat using Black formatter to improve legibility

--- reviews.py
+++ reviews.py
@@ -1,4 +1,4 @@
-from __future__ import print_function # In python 2.7
+from __future__ import print_function  # In python 2.7
 
 from flask import redirect, request, url_for, render_template
 from flask.views import MethodView
@@ -6,6 +6,7 @@ import bgmodel
 import sys
 import requests
 import datetime
+
 
 class Reviews(MethodView):
     def get(self, shop_phone, shop_name):
@@ -18,31 +19,52 @@ class Reviews(MethodView):
         api_keys = dict(google=key_row[0], yelp=key_row[1])
 
         # Retrieve yelp id through phone search
-        response = requests.get("https://api.yelp.com/v3/businesses/search/phone?phone=+1" + shop_phone, 
-            headers={"Authorization": "Bearer " + api_keys['yelp']})
+        response = requests.get(
+            "https://api.yelp.com/v3/businesses/search/phone?phone=+1" + shop_phone,
+            headers={"Authorization": "Bearer " + api_keys["yelp"]},
+        )
 
-        if(response.status_code == 200 and response.json()['total'] > 0):
-            yelp_id = response.json()['businesses'][0]['id']
-        
+        # Check for status code of HTTP request to yelp API
+        if response.status_code == 200 and response.json()["total"] > 0:
+            yelp_id = response.json()["businesses"][0]["id"]
+
             # Retrieve yelp reviews through yelp id and passes them to reviews template
-            raw_reviews = requests.get("https://api.yelp.com/v3/businesses/" + yelp_id + "/reviews", 
-                headers={"Authorization": "Bearer " + api_keys['yelp']}).json()['reviews']
-            
+            raw_reviews = requests.get(
+                "https://api.yelp.com/v3/businesses/" + yelp_id + "/reviews",
+                headers={"Authorization": "Bearer " + api_keys["yelp"]},
+            ).json()["reviews"]
+
             # Format datetime string to mm/dd/yyyy
             for review in raw_reviews:
-                date_time_str = review['time_created']
-                formatted_date = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S').date()
-                review['time_created'] = str(formatted_date.month) + "/" + str(formatted_date.day) + "/" + str(formatted_date.year)
+                date_time_str = review["time_created"]
+                date_str = datetime.datetime.strptime(
+                    date_time_str, "%Y-%m-%d %H:%M:%S"
+                ).date()
+                formatted_date = str(date_str.month) + "/" + str(date_str.day) + "/" + str(date_str.year)
+                review["time_created"] = formatted_date
 
-            yelp_reviews = [dict(id=review['id'], text=review['text'], date=review['time_created'], url=review['url'], 
-                user_name=review['user']['name'], user_img=review['user']['image_url']) for review in raw_reviews]
-            return render_template('reviews.html', yelp_reviews=yelp_reviews, shop_name=shop_name)
+            # Build dictionary of yelp reviews
+            yelp_reviews = [
+                dict(
+                    id=review["id"],
+                    text=review["text"],
+                    date=review["time_created"],
+                    url=review["url"],
+                    user_name=review["user"]["name"],
+                    user_img=review["user"]["image_url"],
+                )
+                for review in raw_reviews
+            ]
+
+            return render_template(
+                "reviews.html", yelp_reviews=yelp_reviews, shop_name=shop_name
+            )
         else:
-            return render_template('404.html', shop_name=shop_name)
+            return render_template("404.html", shop_name=shop_name)
 
     def post(self):
         """
         POST method for the reviews page.
         :return: renders the reivews.html page on return
         """
-        return redirect(url_for('reviews'))
+        return redirect(url_for("reviews"))
